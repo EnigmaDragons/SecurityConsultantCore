ccf5a53577112b8a00ac3cd5d2c48fa4338ec17a
Brendan Harwood <bdharwood@live.com>
2016-09-01 23:31:26 -0700

    Implementing alarms.

---
+++ test/SecurityConsultantCore.Test/Security/ControlPanelTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SecurityConsultantCore.Test.Security
+{
+    [TestClass]
+    public class ControlPanelTests
+    {
+        [TestMethod]
+        public void ControlPanel_Test_Outcome()
+        {
+        }
+    }
+}
