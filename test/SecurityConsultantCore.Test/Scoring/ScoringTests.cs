383899ff3bdf998960060e436f6dfc61dd161495
Tomas <tomizechsterson@gmail.com>
2016-09-08 21:48:57 -0700

    Committing scoring work in progress for now. Need to define incidents before finalizing this since that will play a major role in the final scoring

--- test/SecurityConsultantCore.Test/Scoring/ScoringTests.cs
+++ test/SecurityConsultantCore.Test/Scoring/ScoringTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace SecurityConsultantCore.Test.Scoring
@@ -18,30 +19,63 @@ namespace SecurityConsultantCore.Test.Scoring
         }
 
         [TestMethod]
-        public void Calculate_InvoiceEqualsBudget_ScoreIs0Point5()
+        public void Calculate_InvoiceEqualsBudget_ScoreIs0()
         {
             var score = new Score(1, 1);
 
             double grade = score.Calculate();
 
+            Assert.AreEqual(0.0, grade);
+        }
+
+        [TestMethod]
+        public void Calculate_InvoiceHalfBudget_ScoreIs0Point5()
+        {
+            var score = new Score(1, 0.5);
+
+            double grade = score.Calculate();
+
             Assert.AreEqual(0.5, grade);
+        }
+
+        [TestMethod]
+        public void TestMethod()
+        {
+            WriteToDebug(1.0, 2.0);
+            WriteToDebug(1.0, 1.75);
+            WriteToDebug(1.0, 1.5);
+            WriteToDebug(1.0, 1.25);
+            WriteToDebug(1.0, 1.0);
+            WriteToDebug(1.0, 0.75);
+            WriteToDebug(1.0, 0.5);
+            WriteToDebug(1.0, 0.25);
+        }
+
+        private void WriteToDebug(double budget, double invoiceTotal)
+        {
+            double grade = new Score(budget, invoiceTotal).Calculate();
+            Debug.WriteLine("budget: {0:0.00}\tinvoice: {1:0.00}\tgrade: {2:0.00}", budget, invoiceTotal, grade);
         }
     }
 
+    // Big time w.i.p.. Currently just calculating % of budget used and flooring at 0.00 if over budget (not too fun)
+    // Should also consider details of incident(s) to determine final score. 
+    // Eventually, I think the budget/invoice calculation will amount to more of a modifier on the final score
     public class Score
     {
-        private readonly int _budget;
-        private readonly int _invoiceTotal;
+        private readonly double _budget;
+        private readonly double _invoiceTotal;
 
-        public Score(int budget, int invoiceTotal)
+        public Score(double budget, double invoiceTotal)
         {
             _budget = budget;
             _invoiceTotal = invoiceTotal;
         }
-
+        // Target is 0.0 - 1.0
         public double Calculate()
         {
-            return (_budget - (double)_invoiceTotal) / _budget;
+            double result = (_budget - _invoiceTotal) / _budget;
+            return result < 0.0 ? 0.00 : result;
         }
     }
 }
