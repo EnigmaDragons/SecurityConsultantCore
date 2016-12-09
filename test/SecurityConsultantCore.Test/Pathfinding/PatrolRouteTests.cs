f5e418d162d84743bfe7169ba0efaeb9597f9297
Silas Reinagel <silas.reinagel@gmail.com>
2016-12-08 21:38:11 -0700

    Two files that '-a' did not include

---
+++ test/SecurityConsultantCore.Test/Pathfinding/PatrolRouteTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSTestExtensions;
+using SecurityConsultantCore.Domain.Basic;
+using SecurityConsultantCore.Pathfinding;
+
+namespace SecurityConsultantCore.Test.Pathfinding
+{
+    [TestClass, ExcludeFromCodeCoverage]
+    public class PatrolRouteTests
+    {
+        [TestMethod]
+        public void PatrolRoute_CreateEmptyRoute_ThrowsOnCreation()
+        {
+            ExceptionAssert.Throws<ArgumentException>(() => new PatrolRoute());
+        }
+
+        [TestMethod]
+        public void PatralRoute_SingleSegmentRoutesWithDifferentStartsMatch_False()
+        {
+            var route1 = new PatrolRoute(new Path(new XYZ(1, 1, 1), new XYZ(2, 2, 2)));
+            var route2 = new PatrolRoute(new Path(new XYZ(3, 3, 3), new XYZ(2, 2, 2)));
+
+            Assert.IsFalse(route1.Matches(route2));
+        }
+    }
+}
---
+++ test/SecurityConsultantCore.Test/_TestDoubles/FakeEngineer.cs
@@ -0,0 +1,15 @@
+﻿using SecurityConsultantCore.Pathfinding;
+using SecurityConsultantCore.PlayerCommands;
+
+namespace SecurityConsultantCore.Test._TestDoubles
+{
+    public class FakeEngineer : IEngineer
+    {
+        public PatrolRoute CurrentGuardRoute { get; private set; }
+
+        public void MyPatrolRouteIs(PatrolRoute patrolRoute)
+        {
+            CurrentGuardRoute = patrolRoute;
+        }
+    }
+}
